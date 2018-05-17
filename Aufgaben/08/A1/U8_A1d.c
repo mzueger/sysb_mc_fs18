976fa5e3551aed82592f66599b0204d022a9e08e
Martin Zueger <martin@zueger.eu>
2018-05-17 21:42:07 +0200

    Musterlösung zu Übungsaufgabe 8.1 hinzugefügt

---
+++ Aufgaben/08/A1/U8_A1d.c
@@ -0,0 +1,19 @@
+/*
+ * Übungsaufgabe 8.1d
+ * 
+ */
+
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <pwm.h>
+
+int main() {
+    clock_prescale_set(clock_div_1); // 8 MHz clock
+    pwmInitOC1A(20000, 2000);        // T = 20ms, th = 2ms
+    SREG |= 0x80;                    // Global Interrupt Enable
+
+    while(1) {
+        // nothing to do...
+    }
+    return 0;
+}
