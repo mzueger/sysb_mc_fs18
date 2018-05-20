903c45a2a07bf1bdf2e9d4f5c118ada91b692421
Andreas Kalberer <kaland@gmx.net>
2018-05-20 20:09:18 +0200

    Musterloesung U8_A2 mit Taster S1 und U8_A3c mit langsamer Frequenz

---
+++ Aufgaben/08/A2/U8_A2_ButtonS1.c
@@ -0,0 +1,38 @@
+/*
+ * Uebungsaufgabe 8.2 (Taster S1)
+ * 
+ */
+
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <button.h>
+#include <pwm.h>
+
+#define PERIOD 20000
+#define HIGHTIME_4 (PERIOD / 4)
+
+int main() {
+    static uint8_t f = 1;
+    uint8_t s1Pressed = 0;
+
+    clock_prescale_set(clock_div_1); // 8 MHz clock
+    
+    buttonInit(BUTTON_S1); // Externen Taster initialisieren
+    
+    pwmInitOC1A(PERIOD, 0);          // T = 20ms, th = 0ms
+    
+    SREG |= 0x80;                    // Global Interrupt Enable
+
+    while(1) {
+        if(buttonGet(BUTTON_S1) && !s1Pressed) {
+            pwmUpdateOC1A(PERIOD, HIGHTIME_4 * f);
+            f++;
+            if(f > 3) f = 1;
+            s1Pressed = 1;
+        }
+        else if(!buttonGet(BUTTON_S1)){
+        	s1Pressed = 0;	// reset
+        }
+    }
+    return 0;
+}
---
+++ Aufgaben/08/A3/U8_A3cSlow.c
@@ -0,0 +1,58 @@
+/*
+ * Übungsaufgabe 8.3c
+ * 
+ */
+
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <pwm.h>
+
+#define PERIOD 20000
+#define HIGHTIME_MIN 1200
+#define HIGHTIME_MAX 2200
+
+ISR(TIMER0_COMPA_vect) { // alle 8.064 ms (1 / 8 MHz * 1024 * 32)
+    static uint16_t highTime = HIGHTIME_MIN;
+    static uint8_t dir = 0;
+    
+    TCNT0 = 0;
+    	if(!dir)	// dir == 0: positiv, dir == 1: negative Richtung
+    	{
+    		highTime++;
+    		if(highTime > HIGHTIME_MAX) {
+    			dir = 1;
+    			highTime = HIGHTIME_MAX-1;	// go down first step
+    		}
+    	}
+    	else {	// negativ
+    		highTime--;
+    		if(highTime < HIGHTIME_MIN) {
+    			dir = 0;
+    			highTime = HIGHTIME_MIN+1;	// count up first step
+    		}
+    	}
+        pwmUpdateOC1A(PERIOD, highTime);
+}
+
+void timer0Init() {
+    TCCR0B = 0x00;                       // clear TCCR0B, sets normal timer mode, disable timer
+    TCCR0A = 0x00;                       // clear TCCR0A -> normal port operation, no compare match
+    TCNT0 = 0;                           // clear timer counter value
+    OCR0A = 32;                          // Compare value A
+    TIMSK0 |= (1 << OCIE0A);             // Compare match A interrupt
+    TCCR0B |= (1 << CS02) | (1 << CS00); // clock / 1024 -> start timer
+}
+
+int main() {
+    clock_prescale_set(clock_div_1);     // 8 MHz clock
+    
+    timer0Init();                        // initialize Timer0
+    pwmInitOC1A(PERIOD, HIGHTIME_MIN);   // initialize PWM: T = 20ms, ht = 1.2ms
+    
+    SREG |= 0x80;                        // Global Interrupt Enable
+
+    while(1) {
+        // nothing to do...
+    }
+    return 0;
+}
