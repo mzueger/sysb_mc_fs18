c444935d1fd5442939eba7a75dc1885b502f7011
Martin Zueger <martin@zueger.eu>
2018-05-29 22:36:15 +0200

    Musterlösung zu Aufgabe 8.2 hinzugefügt (noch nicht getestet)

--- Aufgaben/05/sysbavr/adc.c
+++ Aufgaben/05/sysbavr/adc.c
@@ -8,22 +8,24 @@
 #include <adc.h>
 
 void adcInit(int8_t channel) {
-    if(channel < 0 || channel > 13 ||
-       channel == 2 || channel == 3) return;
-
-    ADMUX = channel < 8 ? channel : channel + 24;
-    ADMUX |= (1 << REFS1) | (1 << REFS0);
-
+    if(channel < 0 || channel > 13 || channel == 2 || channel == 3) return;
+    
     ADCSRA = 0;
-    ADCSRA |= (1 << ADEN);
-    ADCSRA |= (1 << ADPS2) | (1 << ADPS1); // div64
     ADCSRB = 0;
-
-    if(channel < 8) DIDR0 = 1 << channel;
-    else DIDR1 = 1 << (channel - 8);
+    
+    ADMUX = channel & 0x07;
+    if(channel >= 8) ADCSRB |= 1 << MUX5;
+    ADMUX |= 1 << REFS0;    // AVCC
+    ADMUX |= ~(1 << REFS1);
+    
+    ADCSRA |= (1 << ADEN); // enable
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1); // div64
+    
+    if(channel < 8) DIDR0 |= 1 << channel;
+    else DIDR2 |= 1 << (channel - 8);
 }
 
-uint16_t adcRead(void) {
+uint16_t adcRead() {
     ADCSRA |= (1 << ADSC);
     while(!(ADCSRA & (1 << ADIF)));
     return (uint16_t)ADCL + (uint16_t)(ADCH << 8);
--- Aufgaben/08/A2/u8a2.c
+++ Aufgaben/08/A2/u8a2.c
@@ -1,12 +1,11 @@
 /*
- * A9 Aufgabe 3
- * a9-3.c
+ * Aufgabe 8.2
  * 
  */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+#include <avr/power.h>
 #include <stdint.h>
 #include <usart.h>
 #include <adc.h>
@@ -14,59 +13,78 @@
 int16_t counter = 0;
 
 ISR(TIMER0_COMPA_vect) {
-	counter ++;
-	if(counter >= 5000) {
-		float r = 330.0 * ((1023.0 / adc_read()) - 1);
-		float t = -1;
-		
-		if(r >= 330.0 && r <424.5) { // 20...25°C
-			t = (r - 424.5) / (330.0 - 424.5) * 5 + 20.0;
-		}
-
-		else if(r >= 258.2 && r < 330.0) { // 25...30°C
-			t = (r - 330.0) / (258.2 - 330.0) * 5 + 25.0;
-		}
-		else if(r >= 203.4 && r < 258.2) { // 30... 35°C
-			t = (r - 258.2) / (203.4 - 258.2) * 5 + 30.0;
-		}
-		else if(r >= 258.2 && r <161.1) { // 35...40°C
-			t = (r - 161.1) / (258.2 - 161.1) * 5 + 35.0;
-		}
-		
-		if(t > 0) {
-			uart_write_string("Temperature: ");
-			int16_t t1 = (int16_t)t;
-			int16_t t2 = (int16_t)((t + 0.005) * 100) - t1;
-			
-			uart_write_int16(t1);
-			uart_write_string(".");
-			uart_write_int16(t2);
-			uart_write_string("°C)\n");
-		}
-		else {
-			uart_write_string("Temperature out of range (R = ");
-			uart_write_int16((int16_t)((r + 0.0005) * 1000));
-			uart_write_string(" mOhm)\n");
-		}
-		counter = 0;
-	}
+    counter ++;
+    if(counter >= 5000) {
+        int16_t v = adcRead();
+        float r = 330.0 * ((1023.0 / v) - 1);
+        float t = -1;
+        
+        if(r >= 1248.0 && r < 1665.0) { // -5...0°C
+            t = (r - 1665.0) / (1248.0 - 1665.0) * 5 - 5.0;
+        }
+        else if(r >= 942.3 && r < 1248.0) { // 0...5°C
+            t = (r - 1248.0) / (942.3 - 1248.0) * 5 + 0.0;
+        }
+        else if(r >= 717.1 && r < 942.3) { // 5...10°C
+            t = (r - 942.3) / (717.1 - 942.3) * 5 + 5.0;
+        }
+        else if(r >= 549.8 && r < 717.1) { // 10...15°C
+            t = (r - 717.1) / (549.8 - 717.1) * 5 + 10.0;
+        }
+        else if(r >= 424.5 && r < 549.8) { // 15...20°C
+            t = (r - 549.8) / (424.5 - 549.8) * 5 + 15.0;
+        }
+        else if(r >= 330.0 && r < 424.5) { // 20...25°C
+            t = (r - 424.5) / (330.0 - 424.5) * 5 + 20.0;
+        }
+        else if(r >= 258.2 && r < 330.0) { // 25...30°C
+            t = (r - 330.0) / (258.2 - 330.0) * 5 + 25.0;
+        }
+        else if(r >= 203.4 && r < 258.2) { // 30... 35°C
+            t = (r - 258.2) / (203.4 - 258.2) * 5 + 30.0;
+        }
+        else if(r >= 258.2 && r < 161.1) { // 35...40°C
+            t = (r - 161.1) / (258.2 - 161.1) * 5 + 35.0;
+        }
+        
+        if(t > 0) {
+            usartWriteString("Temperature: ");
+            int16_t t1 = (int16_t)t;
+            int16_t t2 = (int16_t)((t + 0.005) * 100) - t1;
+            
+            usartWriteInt16(t1);
+            usartWriteString(".");
+            usartWriteInt16(t2);
+            usartWriteString("°C)\n");
+        }
+        else {
+            usartWriteString("Temperature out of range (");
+            usartWriteInt16(v);
+            usartWriteString(" -> ");
+            usartWriteInt16((int16_t)((r + 0.0005) * 1000));
+            usartWriteString(" mOhm)\n");
+        }
+        counter = 0;
+    }
 }
 
 int main(void) {
-	
-	uart_init_96008N1();
-	adc_init(0, ADC_REF_AVCC);
-	
-	TCCR0A = 0x00;
-	TCCR0B = 0x00;
-	TCNT0 = 0;
-	TCCR0A |= 0x2;
-	OCR0A = 125;
-	TIMSK0 |= (1 << OCIE0A);
-	SREG |= 0x80;
-	TCCR0B |= 0x03;
-	
-	while(1) { }
-	
-	return 0;
+    
+    clock_prescale_set(clock_div_1);     // 8 MHz clock
+
+    usartInit96008N1();
+    adcInit(0);
+
+    TCCR0A = 0x00;
+    TCCR0B = 0x00;
+    TCNT0 = 0;
+    TCCR0A |= 0x2;
+    OCR0A = 125;
+    TIMSK0 |= (1 << OCIE0A);
+    SREG |= 0x80;
+    TCCR0B |= 0x03;
+
+    while(1) { }
+
+    return 0;
 }
