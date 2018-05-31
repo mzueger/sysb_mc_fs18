b07386dfd9c7637b2f52d86363396d0fcb0bf7de
Andreas Kalberer <kaland@gmx.net>
2018-05-31 13:27:43 +0200

    ADC Init reference select fixed

--- Aufgaben/05/sysbavr/adc.c
+++ Aufgaben/05/sysbavr/adc.c
@@ -16,7 +16,7 @@ void adcInit(int8_t channel) {
     ADMUX = channel & 0x07;
     if(channel >= 8) ADCSRB |= 1 << MUX5;
     ADMUX |= 1 << REFS0;    // AVCC
-    ADMUX |= ~(1 << REFS1);
+    ADMUX &= ~(1 << REFS1);
     
     ADCSRA |= (1 << ADEN); // enable
     ADCSRA |= (1 << ADPS2) | (1 << ADPS1); // div64
